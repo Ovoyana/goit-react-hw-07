9ea52b85cae29158cbc0e72abf209bc254b2f46f
Ovoyana <olyakovbas@gmail.com>
2024-05-29 19:27:17 +0200

    update ContacsSlice.js

--- src/redux/ContactsSlice.js
+++ src/redux/ContactsSlice.js
@@ -1,5 +1,5 @@
 import { createSelector, createSlice  } from "@reduxjs/toolkit";
-import { fetchContacts, addContact, deleteContact } from "./contactsOps";
+import { fetchContacts, addContact, deleteContact } from '../redux/contactsOps';
 import { selectNameFilter } from './FiltersSlice';
 
 const initialState = {
