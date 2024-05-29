05fb8c66bfda22c27f35e0180587370a3094894c
Ovoyana <olyakovbas@gmail.com>
2024-05-29 17:18:12 +0200

    update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import css from './App.module.css';
-import ContactForm from "../ContactForm/ContactForm.";
-import SearchBox from "../SearchBox/SearchBox.";
-import ContactList from "../ContactList/ContactList.";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactList from "../ContactList/ContactList";
 import { useDispatch, useSelector } from "react-redux";
-import { selectContacts } from '../../redux/contactsSlice.';
+import { selectContacts } from '../../redux/ContactsSlice';
 import { useEffect } from "react";
 import { fetchContacts } from '../../redux/contactsOps';
 import { PiBookOpenTextLight } from "react-icons/pi";
